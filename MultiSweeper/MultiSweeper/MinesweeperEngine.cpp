b45d5a0e6fb6a2cc3e9de5691ce4234ba8a6a7e5
Diogo Silva <dbtdsilva@gmail.com>
2016-08-04 19:39:57 +0100

    Updated engine with some logic

--- MultiSweeper/MultiSweeper/MinesweeperEngine.cpp
+++ MultiSweeper/MultiSweeper/MinesweeperEngine.cpp
@@ -1,7 +1,10 @@
 #include "MinesweeperEngine.h"
 
+int MinesweeperEngine::MAX_PLAYERS = 4;
+
 MinesweeperEngine::MinesweeperEngine()
 {
+	this->currentStatus = START;
 }
 
 
--- MultiSweeper/MultiSweeper/MinesweeperEngine.h
+++ MultiSweeper/MultiSweeper/MinesweeperEngine.h
@@ -5,5 +5,15 @@ class MinesweeperEngine
 public:
 	MinesweeperEngine();
 	virtual ~MinesweeperEngine();
+
+	enum MinesweeperStatus {
+		START,
+		RUN,
+		FINISH
+	};
+	static int MAX_PLAYERS;
+
+private:
+	MinesweeperStatus currentStatus;
 };
 
