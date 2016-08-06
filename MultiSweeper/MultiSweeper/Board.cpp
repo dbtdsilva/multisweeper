494c009dd6117db8d9e29f793a8e8c5e00b254fa
Diogo Silva <dbtdsilva@gmail.com>
2016-08-06 18:25:02 +0100

    Generate mines completed

--- MultiSweeper/MultiSweeper/Board.cpp
+++ MultiSweeper/MultiSweeper/Board.cpp
@@ -1,21 +1,56 @@
 #include "Board.h"
 #include <iostream>
+#include <random>
+#include <time.h>
 
 using namespace std;
 
-Board::Board(int width, int height, int nmines)
+Board::Board(int width, int height, int totalMines)
 {
-	mPos.resize(height);
-	for (int i = 0; i < height; i++) {
-		mPos[i].resize(width, FREE);
-	}
-	mTotalMines = nmines;
-	mWidth = width;
-	mHeight = height;
+	this->modifyBoard(width, height, totalMines);
+	this->generateMines();
 }
 
 Board::~Board()
 {
+}
+
+void Board::generateMines() {
+	if (mTotalMines >= mWidth * mHeight) {
+		return;
+	}
+	clearMines();
+
+	srand(time(NULL));
+	int minesPlaced = 0;
+	int mineW, mineH;
+	while (minesPlaced < mTotalMines) {
+		mineW = rand() % mWidth;
+		mineH = rand() % mHeight;
+
+		if (mPos[mineH][mineW] == FREE) {
+			mPos[mineH][mineW] = MINE;
+			minesPlaced++;
+		}
+	}
+}
+
+void Board::modifyBoard(int width, int height, int totalMines) {
+	this->mPos.resize(height);
+	for (int i = 0; i < height; i++) {
+		this->mPos[i].resize(width, FREE);
+	}
+	this->mTotalMines = totalMines;
+	this->mWidth = width;
+	this->mHeight = height;
+}
+
+void Board::clearMines() {
+	for (int h = 0; h < mHeight; h++) {
+		for (int w = 0; w < mWidth; w++) {
+			mPos[h][w] = FREE;
+		}
+	}
 }
 
 ostream& operator<<(ostream& os, const Board& obj) {
--- MultiSweeper/MultiSweeper/Board.h
+++ MultiSweeper/MultiSweeper/Board.h
@@ -7,19 +7,23 @@ using namespace std;
 class Board
 {
 public:	
-	Board(int width, int height, int nmines);
+	Board(int width, int height, int totalMines);
 	virtual ~Board();
+
+	void generateMines();
+	void modifyBoard(int width, int height, int totalMines);
 	friend std::ostream& operator<<(std::ostream& os, const Board& obj);
 
 	enum Position {
 		FREE,
 		MINE,
 		FREE_DISCOVERED,
-		MINE_DISCOVERED,
-		MINE_DESTROYED
+		MINE_DISCOVERED
 	};
 
 private:
+	void clearMines();
+
 	std::vector<std::vector<Position> > mPos;
 	int mHeight;
 	int mWidth;
