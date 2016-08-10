0702e2ec5906e6a1b038529f1fd1b5ae13699d91
Diogo Silva <dbtdsilva@gmail.com>
2016-08-11 00:30:15 +0100

    Added PDCurses34.zip and MultiSweeperConsole main

--- MultiSweeper/MultiSweeperConsole/MultiSweeperConsole.cpp
+++ MultiSweeper/MultiSweeperConsole/MultiSweeperConsole.cpp
@@ -9,6 +9,8 @@ int main(int argc, char **argv)
 {
 	unique_ptr<SweeperConsole> pSweeperConsole = unique_ptr<SweeperConsole>(new SweeperConsole());
 	unique_ptr<Engine> pEngine = unique_ptr<Engine>(new Engine(pSweeperConsole.get()));
+
+
 	cout << "Press any key to exit..." << flush;
 	cin.get();
 	return 0;
