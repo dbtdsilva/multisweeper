d7bd70ffab381b876a679ccb500fe5a3bec9bed5
Diogo Silva <dbtdsilva@gmail.com>
2016-08-23 18:11:35 +0100

    Updated SweeperError

--- MultiSweeper/MultiSweeper/SweeperError.cpp
+++ MultiSweeper/MultiSweeper/SweeperError.cpp
@@ -1,13 +1,7 @@
 #include "SweeperError.h"
 
-const SweeperError SweeperError::OK(0, "Success");
-
 SweeperError::SweeperError(int error_code, std::string message) :
 	error_code_(error_code), message_(message)
-{
-}
-
-SweeperError::~SweeperError()
 {
 }
 
@@ -18,3 +12,10 @@ std::string const& SweeperError::get_message() const {
 int const& SweeperError::get_error_code() const {
 	return error_code_;
 }
+
+const SweeperError SweeperError::SUCCESS(0, "Success");
+const SweeperError SweeperError::NO_PLAYERS(1, "There is no players registered");
+const SweeperError SweeperError::GAME_ALREADY_STARTED(2, "Game has already started");
+const SweeperError SweeperError::GAME_NOT_RUNNING(3, "Game is not running");
+const SweeperError SweeperError::POSITION_ALREADY_REVEALED(4, "Position was already revealed");
+const SweeperError SweeperError::MINES_EXCEED(5, "There is more mines than board fields");
--- MultiSweeper/MultiSweeper/SweeperError.h
+++ MultiSweeper/MultiSweeper/SweeperError.h
@@ -2,16 +2,23 @@
 
 #include <string>
 
+using namespace std;
+
 class SweeperError
 {
 public:
-	static const SweeperError OK;
+	static const SweeperError SUCCESS;
+	static const SweeperError NO_PLAYERS;
+	static const SweeperError GAME_ALREADY_STARTED;
+	static const SweeperError GAME_NOT_RUNNING;
+	static const SweeperError POSITION_ALREADY_REVEALED;
+	static const SweeperError MINES_EXCEED;
 	
 	std::string const& get_message() const;
 	int const& get_error_code() const;
+
 private:
 	SweeperError(int error_code, std::string message);
-	~SweeperError();
 
 	int error_code_;
 	std::string message_;
